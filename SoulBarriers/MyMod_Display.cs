f703dcd6b4290f8ffd1f1f76be8d50e488140766
hamstar0 <webutek@gmail.com>
2021-07-31 04:35:05 -0700

    v0.19.1 - Implemented world barrier hover stats

--- SoulBarriers/MyMod_Display.cs
+++ SoulBarriers/MyMod_Display.cs
@@ -7,49 +7,100 @@ using ModLibsCore.Libraries.DotNET.Extensions;
 using SoulBarriers.Barriers;
 using SoulBarriers.Barriers.BarrierTypes;
 using SoulBarriers.Barriers.BarrierTypes.Spherical;
-
+using SoulBarriers.Barriers.BarrierTypes.Rectangular;
+using ModLibsCore.Libraries.Debug;
 
 namespace SoulBarriers {
 	public partial class SoulBarriersMod : Mod {
+		public static (string stats, Vector2 dim) GetBarrierStatsData( Barrier barrier ) {
+			string stats = barrier.Strength + " hp";
+			Vector2 statsDim = Main.fontMouseText.MeasureString( stats );
+
+			return (stats, statsDim);
+		}
+
+
+
+		////////////////
+
 		public override void PostDrawInterface( SpriteBatch sb ) {
 			foreach( (int plrWho, Barrier barrier) in BarrierManager.Instance.GetPlayerBarriers() ) {
 				if( barrier.Strength <= 0 ) {
 					continue;
 				}
-				if( !(barrier is SphericalBarrier) ) {
+
+				Player plr = Main.player[plrWho];
+
+				if( barrier is SphericalBarrier ) {
+					this.DisplayPlayerBarrierStatsIf( sb, barrier as SphericalBarrier, plr );
+				}
+			}
+
+			foreach( Barrier barrier in BarrierManager.Instance.GetWorldBarriers().Values ) {
+				if( barrier.Strength <= 0 ) {
 					continue;
 				}
 
-				Player plr = Main.player[ plrWho ];
-				Vector2 barrierPos = barrier.GetBarrierWorldCenter();
-				float radius = ((SphericalBarrier)barrier).Radius;
-
-				if( (barrierPos - Main.MouseWorld).LengthSquared() < (radius * radius) ) {
-					this.DisplayBarrierStats( sb, plr, barrier, radius );
+				if( barrier is RectangularBarrier ) {
+					this.DisplayRectangularBarrierStatsIf( sb, barrier as RectangularBarrier );
 				}
 			}
 		}
 
 
-		////
+		////////////////
 
-		private void DisplayBarrierStats( SpriteBatch sb, Player player, Barrier barrier, float radius ) {
-			string stats = barrier.Strength+" hp";
-			Vector2 statsDim = Main.fontMouseText.MeasureString( stats );
+		private void DisplayPlayerBarrierStatsIf( SpriteBatch sb, SphericalBarrier barrier, Player plr ) {
+			Vector2 barrierPos = barrier.GetBarrierWorldCenter();
+			float radius = ((SphericalBarrier)barrier).Radius;
 
-			Vector2 pos = player.MountedCenter;
-			pos.Y -= radius + (statsDim.Y * 1.5f);
+			if( (barrierPos - Main.MouseWorld).LengthSquared() < (radius * radius) ) {
+				this.DisplayPlayerSphereBarrierStats( sb, plr, barrier );
+			}
+		}
+		
+		private void DisplayRectangularBarrierStatsIf( SpriteBatch sb, RectangularBarrier barrier ) {
+			if( !barrier.WorldArea.Contains(Main.MouseWorld.ToPoint()) ) {
+				return;
+			}
+
+			Vector2 worldPos = Main.MouseWorld + new Vector2(0f, -28f);
+			(string stats, Vector2 dim) stats = SoulBarriersMod.GetBarrierStatsData( barrier );
+
+			this.DisplayBarrierStats( sb, worldPos, stats, barrier.BarrierColor );
+		}
+
+
+		////////////////
+
+		private void DisplayPlayerSphereBarrierStats( SpriteBatch sb, Player player, SphericalBarrier barrier ) {
+			(string stats, Vector2 dim) stats = SoulBarriersMod.GetBarrierStatsData( barrier );
+
+			Vector2 worldPos = player.MountedCenter;
+			worldPos.Y -= barrier.Radius + (stats.dim.Y * 1.5f);
 			//pos.X -= statsDim.X * 0.5f;
 
+			this.DisplayBarrierStats( sb, worldPos, stats, barrier.BarrierColor );
+		}
+
+
+		////////////////
+
+		private void DisplayBarrierStats(
+					SpriteBatch sb,
+					Vector2 worldPos,
+					(string stats, Vector2 dim) stats,
+					BarrierColor color ) {
+DebugLibraries.Print( "barrier stats", worldPos.ToString()+", "+(worldPos-Main.screenPosition) );
 			Utils.DrawBorderStringFourWay(
 				sb: sb,
 				font: Main.fontMouseText,
-				text: stats,
-				x: pos.X - Main.screenPosition.X,
-				y: pos.Y - Main.screenPosition.Y,
-				textColor: Barrier.GetColor( barrier.BarrierColor ) * ((float)Main.mouseTextColor / 255f),
+				text: stats.stats,
+				x: worldPos.X - Main.screenPosition.X,
+				y: worldPos.Y - Main.screenPosition.Y,
+				textColor: Barrier.GetColor(color) * ((float)Main.mouseTextColor / 255f),
 				borderColor: Color.Black,
-				origin: statsDim * 0.5f
+				origin: stats.dim * 0.5f
 			);
 		}
 	}
