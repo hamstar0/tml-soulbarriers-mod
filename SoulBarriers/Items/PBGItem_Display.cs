2bf2da8d763380bddddf0f5aef49986d78e43857
hamstar0 <webutek@gmail.com>
2021-08-31 05:39:39 -0700

    v0.23.0 - Added barrier cooldown duration tooltip

--- SoulBarriers/Items/PBGItem_Display.cs
+++ SoulBarriers/Items/PBGItem_Display.cs
@@ -10,40 +10,46 @@ using SoulBarriers.Buffs;
 
 namespace SoulBarriers.Items {
 	public partial class PBGItem : ModItem {
-		public override void ModifyTooltips( List<TooltipLine> tooltips ) {
-			var myplayer = Main.LocalPlayer.GetModPlayer<SoulBarriersPlayer>();
+		public static string GetColorCode( int range, float percent ) {
+			Color c;
 
-			double str = myplayer.Barrier.Strength;
-			/*int maxStr = myplayer.Barrier.MaxRegenStrength;
-
-			string msg = str.ToString();
-			if( maxStr >= 1 && str > maxStr ) {
-				msg = "+" + msg;
+			switch( range ) {
+			case 0:
+				return "444444";
+			case 1:
+				c = Color.Lerp( Color.Red, Color.Yellow, percent );
+				return MiscLibraries.RenderColorHex( c );
+			case 2:
+				c = Color.Lerp( Color.Yellow, Color.Lime, percent );
+				return MiscLibraries.RenderColorHex( c );
+			case 3:
+				c = Color.Lerp( Color.Lime, Color.Blue, percent );
+				return MiscLibraries.RenderColorHex( c );
+			case 4:
+				c = Color.Lerp( Color.Blue, Color.Magenta, percent );
+				return MiscLibraries.RenderColorHex( c );
+			default:
+				return MiscLibraries.RenderColorHex( Color.Cyan );
 			}
-			string postMsg = maxStr >= 1 && str <= maxStr
-				? " of "+maxStr
-				: "";*/
+		}
 
-			string colorCode;
-			if( str <= 0 ) {
-				colorCode = "444444";
-			} else if( str <= 50d ) {
-				Color c = Color.Lerp( Color.Red, Color.Yellow, (float)(str / 50d) );
-				colorCode = MiscLibraries.RenderColorHex( c );
-			} else if( str <= 100d ) {
-				double sStr = str - 50d;
-				Color c = Color.Lerp( Color.Yellow, Color.Lime, (float)(sStr / 50d) );
-				colorCode = MiscLibraries.RenderColorHex( c );
-			} else if( str <= 150 ) {
-				double sStr = str - 100d;
-				Color c = Color.Lerp( Color.Lime, Color.Blue, (float)(sStr / 50d) );
-				colorCode = MiscLibraries.RenderColorHex( c );
-			} else if( str <= 200 ) {
-				double sStr = str - 150d;
-				Color c = Color.Lerp( Color.Lime, Color.Magenta, (float)(sStr / 50d) );
-				colorCode = MiscLibraries.RenderColorHex( c );
+		public static double GetDisplayStrength( double strength, out string colorCode ) {
+			if( strength <= 0 ) {
+				colorCode = PBGItem.GetColorCode( 0, 1f );
+			} else if( strength <= 50d ) {
+				double sStr = strength;
+				colorCode = PBGItem.GetColorCode( 1, (float)(sStr / 50d) );
+			} else if( strength <= 100d ) {
+				double sStr = strength - 50d;
+				colorCode = PBGItem.GetColorCode( 2, (float)(sStr / 50d) );
+			} else if( strength <= 150d ) {
+				double sStr = strength - 100d;
+				colorCode = PBGItem.GetColorCode( 3, (float)(sStr / 50d) );
+			} else if( strength <= 200d ) {
+				double sStr = strength - 150d;
+				colorCode = PBGItem.GetColorCode( 4, (float)(sStr / 50d) );
 			} else {
-				colorCode = MiscLibraries.RenderColorHex( Color.Cyan );
+				colorCode = PBGItem.GetColorCode( 5, 1f );
 			}
 
 			/*} else if( str < maxStr ) {
@@ -54,15 +60,71 @@ namespace SoulBarriers.Items {
 				colorCode = "44DDFF";//MiscLibraries.RenderColorHex( Color.White );
 			}*/
 
-			var tip1 = new TooltipLine(
-				this.mod,
+			//
+
+			double dispStrength = (int)strength;
+			int percent = (int)( ( strength % 1d ) * 100d );
+			dispStrength += (double)percent / 100d;
+
+			return dispStrength;
+		}
+
+
+		////////////////
+
+		public static (bool success, string id, string text) GetBarrierStrengthTipTexts() {
+			var myplayer = Main.LocalPlayer.GetModPlayer<SoulBarriersPlayer>();
+
+			double strength = PBGItem.GetDisplayStrength( myplayer.Barrier.Strength, out string colorCode );
+
+			return (
+				true,
 				"PlayerBarrierStrength",
-				"Current barrier strength: [c/"+colorCode+":"+str+"]"
+				" Barrier strength: [c/" + colorCode + ":" + strength + "]"
 			);
+		}
+
+		public static (bool success, string id, string text) GetBarrierCooldownTipTexts() {
+			int buffIdx = Main.LocalPlayer.FindBuffIndex( ModContent.BuffType<PBGOverheatedDeBuff>() );
+			if( buffIdx == -1 ) {
+				return (false, null, null);
+			}
+
+			int buffTime = Main.LocalPlayer.buffTime[buffIdx];
+			string buffTimeDisp = MiscLibraries.RenderTickDuration( buffTime );
+
+			var config = SoulBarriersConfig.Instance;
+			int seconds = config.Get<int>( nameof(config.PBGOverheatDurationSeconds) );
+			int ticks = seconds * 60;
+			string colorCode = PBGItem.GetColorCode( 1, (float)buffTime / (float)ticks );
+
+			return (
+				true,
+				"PlayerBarrierCooldown",
+				" Barrier cooldown: [c/" + colorCode + ":" + buffTimeDisp + "]"
+			);
+		}
+
+
+
+		////////////////
+
+		public override void ModifyTooltips( List<TooltipLine> tooltips ) {
+			(bool success, string id, string text) tipData;
 
 			//
 
-			tooltips.Add( tip1 );
+			tipData = PBGItem.GetBarrierStrengthTipTexts();
+			if( tipData.success ) {
+				var tip = new TooltipLine( this.mod, tipData.id, tipData.text );
+				tooltips.Add( tip );
+			}
+
+			tipData = PBGItem.GetBarrierCooldownTipTexts();
+			if( tipData.success ) {
+				var tip = new TooltipLine( this.mod, tipData.id, tipData.text );
+				tooltips.Add( tip );
+			}
 		}
 
 
