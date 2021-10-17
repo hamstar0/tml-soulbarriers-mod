14b614e48f69f11453e333a75c2bf2a4d3f2e598
hamstar0 <webutek@gmail.com>
2021-10-17 15:31:08 -0700

    restructured some fx code files

--- SoulBarriers/Barriers/BarrierTypes/Barrier_Fx_Animate.cs
+++ SoulBarriers/Barriers/BarrierTypes/Barrier_Fx_Animate.cs
@@ -53,30 +53,5 @@ namespace SoulBarriers.Barriers.BarrierTypes {
 
 			return true;
 		}
-
-
-		////////////////
-
-		public virtual int ComputeNormalParticleCount() {
-			if( this.Strength <= 0 ) {
-				return 0;
-			}
-
-			int particlesViaStr = (int)(this.Strength / 3d);
-			int maxParticles = Math.Min( particlesViaStr, Barrier.MaximumNormalParticles );
-
-			return Math.Max( Barrier.MinimumNormalParticles, maxParticles );
-		}
-
-		public virtual int ComputeHitParticleCountMax() {
-			double maxStr = !this.MaxRegenStrength.HasValue || this.MaxRegenStrength.Value <= 0d
-				? this.InitialStrength
-				: this.MaxRegenStrength.Value;
-			
-			int particlesViaStr = (int)(maxStr / 3d);
-			int maxParticles = Math.Min( particlesViaStr, Barrier.MaximumHitParticles );
-
-			return Math.Max( Barrier.MinimumHitParticles, maxParticles );
-		}
 	}
 }
---
+++ SoulBarriers/Barriers/BarrierTypes/Barrier_Fx_Particles_Compute.cs
@@ -0,0 +1,29 @@
+using System;
+using Terraria;
+
+
+namespace SoulBarriers.Barriers.BarrierTypes {
+	public abstract partial class Barrier {
+		public virtual int ComputeNormalParticleCount() {
+			if( this.Strength <= 0 ) {
+				return 0;
+			}
+
+			int particlesViaStr = (int)(this.Strength / 3d);
+			int maxParticles = Math.Min( particlesViaStr, Barrier.MaximumNormalParticles );
+
+			return Math.Max( Barrier.MinimumNormalParticles, maxParticles );
+		}
+
+		public virtual int ComputeHitParticleCountMax() {
+			double maxStr = !this.MaxRegenStrength.HasValue || this.MaxRegenStrength.Value <= 0d
+				? this.InitialStrength
+				: this.MaxRegenStrength.Value;
+			
+			int particlesViaStr = (int)(maxStr / 3d);
+			int maxParticles = Math.Min( particlesViaStr, Barrier.MaximumHitParticles );
+
+			return Math.Max( Barrier.MinimumHitParticles, maxParticles );
+		}
+	}
+}
