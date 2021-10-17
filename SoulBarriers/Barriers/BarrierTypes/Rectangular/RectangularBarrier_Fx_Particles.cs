2921effa99f793d3e66d5f238d1d2fa545d8fa20
hamstar0 <webutek@gmail.com>
2021-10-17 15:33:05 -0700

    more code file restructuring

---
+++ SoulBarriers/Barriers/BarrierTypes/Rectangular/RectangularBarrier_Fx_Particles.cs
@@ -0,0 +1,40 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using SoulBarriers.Dusts;
+
+
+namespace SoulBarriers.Barriers.BarrierTypes.Rectangular {
+	public partial class RectangularBarrier : Barrier {
+		public bool DecideIfParticleTooFarAwayForFx( Vector2 offset ) {
+			// any distance is fine, 10% of the time
+			if( Main.rand.NextFloat() <= 0.1f ) {
+				return false;
+			}
+
+			Vector2 worldPos = offset + this.GetBarrierWorldCenter();
+			float distSqr = (Main.LocalPlayer.MountedCenter - worldPos).LengthSquared();
+
+			float maxRandDist = 12f * 16f;	// 12 tiles
+			float maxRandDistSqr = maxRandDist * maxRandDist;
+
+
+			return distSqr > Main.rand.NextFloat( maxRandDistSqr );
+		}
+
+
+		////////////////
+
+		public override Dust CreateBarrierParticleAt( Vector2 position, bool isHit ) {
+			return BarrierDust.Create(
+				source: this,
+				position: position,
+				color: this.Color,
+				isHit: isHit,
+				durationPercentPerTick: isHit
+					? BarrierDust.DefaultPercentDurationElapsedPerTick * 0.25f
+					: BarrierDust.DefaultPercentDurationElapsedPerTick * 0.5f
+			);
+		}
+	}
+}
